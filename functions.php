46a1a5ead90b92d48adae24cda199b38ee3be4bd
Pham Duc Tien <ptiencntt94@gmail.com>
2020-02-03 16:01:25 +0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+/**
+	 * Hex to RGBA
+	 *
+	 * @param string $hex string hex code.
+	 * @param number $alpha alpha number.
+	 */
+	function hex2rgba( $hex, $alpha ) {
+		$hex = str_replace( '#', '', $hex );
+
+		if ( strlen( $hex ) == 3 ) {
+			$r = hexdec( substr( $hex, 0, 1 ) . substr( $hex, 0, 1 ) );
+			$g = hexdec( substr( $hex, 1, 1 ) . substr( $hex, 1, 1 ) );
+			$b = hexdec( substr( $hex, 2, 1 ) . substr( $hex, 2, 1 ) );
+		} else {
+			$r = hexdec( substr( $hex, 0, 2 ) );
+			$g = hexdec( substr( $hex, 2, 2 ) );
+			$b = hexdec( substr( $hex, 4, 2 ) );
+		}
+		$rgba = 'rgba(' . $r . ', ' . $g . ', ' . $b . ', ' . $alpha . ')';
+		return $rgba;
+	}
